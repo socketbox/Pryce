71c827c3c8a050f21dd49ef3279b7475ffb4bd93
Khoa 'Mike' Phan <khoaphan90@gmail.com>
2020-02-20 18:52:45 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { createAppContainer } from 'react-navigation';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducers from './components/account/reducers/index'
@@ -7,7 +8,7 @@ import AppNav from './components/AppNavigator';
 
 const store = createStore(reducers);
 
-export default class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
@@ -16,3 +17,5 @@ export default class App extends Component {
     )
   }
 }
+
+export default App;
