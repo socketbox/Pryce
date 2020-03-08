03e6d288ff302632f17562d4b219d4466d3ae7a9
Khoa 'Mike' Phan <khoaphan90@gmail.com>
2020-03-08 13:23:33 -0400

    itemdetail fix

---
+++ components/item/ItemDetail.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import {
+	Text,
+	View,
+	Alert,
+	TouchableOpacity,
+} from 'react-native';
+import { DataTable } from 'react-native-paper';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { styles } from '../Styles';
+import {
+	DataTablePagination,
+	TextField,
+	Icon,
+	IconButton,
+	Card,
+	CardHeader,
+} from 'material-bread';
+
+export default class ItemDetail extends React.Component {
+
+}
