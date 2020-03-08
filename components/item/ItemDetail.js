6efb2271cc36c63cca1fcfbeef47ecfe4df8c5df
Khoa 'Mike' Phan <khoaphan90@gmail.com>
2020-03-08 14:17:50 -0400

    Added itemDetail screen

--- components/item/ItemDetail.js
+++ components/item/ItemDetail.js
@@ -2,10 +2,9 @@ import * as React from 'react';
 import {
 	Text,
 	View,
-	Alert,
 	TouchableOpacity,
 } from 'react-native';
-import { DataTable } from 'react-native-paper';
+import { Card, DataTable } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { styles } from '../Styles';
 import {
@@ -13,10 +12,48 @@ import {
 	TextField,
 	Icon,
 	IconButton,
-	Card,
 	CardHeader,
 } from 'material-bread';
 
 export default class ItemDetail extends React.Component {
+    state = {
+        priceID: this.props.navigation.getParam('item', 'null'),
+        itemData: this.props.navigation.getParam('itemData', 'null'),
+    }
 
+    _goToStore(storeInfo) {
+        // add store to search stack index for this to work!
+        console.log("pressed");
+        this.props.navigation.navigate('Store', { storeInfo })
+    }
+
+    _addToList(){
+
+    }
+
+    render() {
+        let title = this.state.itemData.brand + ' - ' + this.state.itemData.name;
+        let storeInfo = this.state.priceID.store.place_id;
+        return (
+            <View style={styles.mainContainer}>
+                <Card>
+                    <Card.Title
+                        titleStyle={{ fontSize: 25 }}
+                        wrapperStyle={true}
+                        title={title}
+                    />
+                    <Card.Content>
+                        <Text style={styles.textStyleSmall}>Located at: </Text>
+                        <TouchableOpacity onPress={() => this._goToStore(storeInfo)} style={styles.button}>
+                            <Text style={styles.buttonText, {fontSize: 25}}>{this.state.priceID.store.name}</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={this._addToList} style={styles.button}>
+                            <Text style={styles.buttonText, {fontSize: 25}}>Add item to list!</Text>
+                        </TouchableOpacity>
+                        <Text style={styles.textStyleSmall}>Price: {this.state.priceID.price}</Text>
+                    </Card.Content>
+                </Card>
+            </View>
+        );
+    }
 }
